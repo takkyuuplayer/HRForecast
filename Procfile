ba9043fcdd6f6af9ea94f218d0e2caa1c795eebd
takkyuuplayer <takkyuuplayer@gmail.com>
2015-01-17 17:26:39 +0000

    Add script to run on heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web; carton exec -- perl -Ilib hrforecast.pl --config config.pl
--- config.pl
+++ config.pl
@@ -1,10 +1,27 @@
-{
-    dsn => 'dbi:mysql:hrforecast;hostname=127.0.0.1',
-    username => '',
+my $c = {
+    username => 'root',
     password => '',
-    port => '5127',
-    host => '127.0.0.1',
-    front_proxy => [],
-    allow_from => [],
+    hostname => '127.0.0.1',
+    database => 'hrforecast',
+    dsn      => 'dbi:mysql:hrforecast;hostname=127.0.0.1',
 };
+if ($ENV{CLEARDB_DATABASE_URL} =~ m,mysql://(.*):(.*)@(.*)/(.*),) {
+    my ($username, $password, $hostname, $database) = ($1, $2, $3, $4);
+    $c->{username} = $username;
+    $c->{password} = $password;
+    $c->{hostname} = $hostname;
+    $c->{database} = $database;
+    $database =~ s/(.*)\?(.*)/$1/;
+    $c->{dsn} = sprintf "dbi:mysql:database=%s:host=%s", $database, $hostname;
+}
 
+%conf = (
+    %$c,
+    (    #port => '80',
+            #host => '127.0.0.1',
+        front_proxy => [],
+        allow_from  => [],
+    )
+);
+
+\%conf;
